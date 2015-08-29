250a3d7ba0239e58ac8557b09fa501bd6e9bb5d2
Alejo Pacin Jul <alejopj@gmail.com>
2015-08-29 19:09:00 +0200

    Added complete support for real samples.

--- src/gui/prototipo4/src/main/model/entities/MidiServerUnix.java
+++ src/gui/prototipo4/src/main/model/entities/MidiServerUnix.java
@@ -1,9 +1,12 @@
 package main.model.entities;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import main.model.utils.FileUtils;
 import main.model.utils.MidiUtils;
 
 public class MidiServerUnix extends MidiServer {
@@ -13,6 +16,9 @@ public class MidiServerUnix extends MidiServer {
 	private static final String TEMP_PATH = "/tmp";
 	private static final String VIBRATO = "--vibrato";
 	private static final String CHORUS = "--chorus=n";
+	private static final String SF2_FILE_PATH = "sounds/FluidR3_GM.sf2";
+	private static final String DEF_CONFIG_FILE_PATH =
+			"/etc/timidity/timidity.cfg";
 	
 	private static String configFilePath;
 	private static String tablePath;
@@ -32,12 +38,16 @@ public class MidiServerUnix extends MidiServer {
 		// Real samples.
 		if (configuration.useRealSamples()) {
 			configFilePath = getRealSamplesConfigFilePath();
-			command.add(REAL_SAMPLES + configFilePath);
+			if (configFilePath != null) {
+				command.add(REAL_SAMPLES + configFilePath);
+			}
 		}
 		
 		// Tuning mode, tuning frequency and precise tunings.
 		tablePath = getFrequencyTablePath(configuration);
-		command.add(FREQ_TABLE + tablePath);
+		if (tablePath != null) {
+			command.add(FREQ_TABLE + tablePath);
+		}
 		
 		// Continuous vibrato.
 		if (configuration.useContinuousVibrato()) {
@@ -49,19 +59,93 @@ public class MidiServerUnix extends MidiServer {
 		return command;
 	}
 	
+	/**
+	 * Configure the MIDI server configuration file to use real samples
+	 * returning the path to the custom configuration file to use.
+	 * @return The path to custom configuration file to use.
+	 */
 	private String getRealSamplesConfigFilePath() {
 		
 		String realSamplesConfigFilePath = null;
 		
-		// TODO Implement.
-		// Copy the bagpipe.sf2 to /tmp.
-		// Copy the timidity.cfg of the system to /tmp.
-		// Modify the timidity.cfg to include the sf2 file.
-		// Return the path to this config file.
+		try {
+			// Copy the SoundFont file to the temporal path.
+			// TODO Test this line carefully. SF2 file could not be found this way.
+			String soundFontFilePath =
+					FileUtils.copyFileToDirectory(SF2_FILE_PATH, TEMP_PATH);
+			// Copy the timidity.cfg of the system to the temporal path.
+			realSamplesConfigFilePath = 
+				FileUtils.copyFileToDirectory(DEF_CONFIG_FILE_PATH, TEMP_PATH);
+			if (realSamplesConfigFilePath != null) {
+				// Modify the timidity.cfg to use the SoundFont file.
+				setSoundFontSource(realSamplesConfigFilePath,
+						soundFontFilePath);
+			}
+		} catch (IOException e) {
+			realSamplesConfigFilePath = null;
+			System.err.println(
+					"Error while getting the real samples configuration file path." +
+					" Message: " + e.getMessage());
+			e.printStackTrace();
+		}
 		
 		return realSamplesConfigFilePath;
 	}
 	
+	/**
+	 * Configure the MIDI server configuration file to use the provided
+	 * SoundFont source file.
+	 * @param configFilePath Path to the MIDI server configuration file.
+	 * @param soundFontFilePath Path to the SoundFont source file.
+	 * @return A boolean indicating if the configuration was fine.
+	 * @throws IOException If a problem comes up while managing the
+	 * configuration file.
+	 */
+	private boolean setSoundFontSource(String configFilePath,
+			String soundFontFilePath) throws IOException {
+		
+		boolean  isSet = false;
+		
+		File configFile = new File(configFilePath);
+		try {
+			List<String> lines = FileUtils.readLines(configFile);
+			
+			// Disable other sound sources.
+			for (String line : lines) {
+				if (line.startsWith("source")) {
+					line = "#" + line;
+				}
+			}
+			
+			// Enable custom SoundFont source.
+			String soundFontDirectory =
+					soundFontFilePath.substring(0,
+							soundFontFilePath.lastIndexOf("/")+1);
+			String soundFontFile = soundFontFilePath.substring(
+					soundFontFilePath.lastIndexOf("/"));
+			lines.add("dir " + soundFontDirectory);
+			lines.add("soundfont " + soundFontFile);
+			
+			FileUtils.writeLines(configFile, lines, false);
+			
+			isSet = true;
+		} catch (IOException e) {
+			System.err.println(
+					"Error while setting the SoundFont source." +
+					" Message: " + e.getMessage());
+			e.printStackTrace();
+			throw e;
+		}
+		
+		return isSet;
+	}
+	
+	/**
+	 * Generate a custom tuning frequency table based on the provided
+	 * configuration returning the path to the file containing the table.
+	 * @param configuration MIDI server configuration.
+	 * @return The path to file containing the frequency table.
+	 */
 	private String getFrequencyTablePath(
 			MidiServerConfiguration configuration) {
 		

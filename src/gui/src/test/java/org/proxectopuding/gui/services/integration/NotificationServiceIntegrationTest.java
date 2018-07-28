74e2d3315ff0bfefab623b2a16380c89e77fe8c2
Alejo Pacin Jul <alejopj@gmail.com>
2018-07-28 20:21:19 +0200

    Fix integration tests

--- src/gui/src/test/java/org/proxectopuding/gui/services/integration/NotificationServiceIntegrationTest.java
+++ src/gui/src/test/java/org/proxectopuding/gui/services/integration/NotificationServiceIntegrationTest.java
@@ -1,15 +1,8 @@
 package org.proxectopuding.gui.services.integration;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.proxectopuding.gui.model.services.NotificationService;
 import org.proxectopuding.gui.model.services.impl.NotificationServiceImpl;
@@ -25,60 +18,35 @@ public class NotificationServiceIntegrationTest {
 	private static final PropertyChangeListener LISTENER = 
 			getPropertyChangeListener();
 	
-	private NotificationManager notificationManager =
-			mock(NotificationManager.class);
+	private NotificationManager notificationManager = new NotificationManager();
 	private NotificationService notificationService =
 			new NotificationServiceImpl(notificationManager);
 
-	@Before
-	public void before() {
-		
-		reset(notificationManager);
-	}
-	
 	@Test
 	public void sendNotification() {
 		
 		// Given
-		doNothing().when(notificationManager)
-				.sendNotification(SOURCE, PROPERTY_NAME.name(), NEW_VALUE);
+		notificationService.addNotificationListener(PROPERTY_NAME, LISTENER);
 		
 		// When
 		notificationService.sendNotification(SOURCE, PROPERTY_NAME, NEW_VALUE);
-		
-		// Then
-		verify(notificationManager, times(1))
-				.sendNotification(SOURCE, PROPERTY_NAME.name(), NEW_VALUE);
 	}
 	
 	@Test
 	public void addNotificationListener() {
 		
-		// Given
-		doNothing().when(notificationManager)
-				.addNotificationListener(PROPERTY_NAME.name(), LISTENER);
-		
 		// When
 		notificationService.addNotificationListener(PROPERTY_NAME, LISTENER);
-		
-		// Then
-		verify(notificationManager, times(1))
-				.addNotificationListener(PROPERTY_NAME.name(), LISTENER);
 	}
 
 	@Test
 	public void removeNotificationListener() {
 		
 		// Given
-		doNothing().when(notificationManager)
-				.removeNotificationListener(PROPERTY_NAME.name(), LISTENER);
+		notificationService.addNotificationListener(PROPERTY_NAME, LISTENER);
 		
 		// When
 		notificationService.removeNotificationListener(PROPERTY_NAME, LISTENER);
-		
-		// Then
-		verify(notificationManager, times(1))
-				.removeNotificationListener(PROPERTY_NAME.name(), LISTENER);
 	}
 	
 	private static PropertyChangeListener getPropertyChangeListener() {

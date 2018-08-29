1c6f01c2f1b590e81641c46ddb16f2f6e94b9953
Alejo Pacín Jul <alejopj@gmail.com>
2018-08-30 00:58:53 +0200

    Add acceptance tests to doc

---
+++ src/gui/src/test/java/org/proxectopuding/gui/services/acceptance/ConfigurationApplicationServiceAcceptanceMockTest.java
@@ -0,0 +1,91 @@
+package org.proxectopuding.gui.services.acceptance;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServer;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServerConfiguration;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServerGeneral;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServerUnix;
+import org.proxectopuding.gui.model.services.ConfigurationApplicationService;
+import org.proxectopuding.gui.model.services.DeviceManagerService;
+import org.proxectopuding.gui.model.services.MidiService;
+import org.proxectopuding.gui.model.services.impl.ConfigurationApplicationServiceImpl;
+import org.proxectopuding.gui.model.services.impl.MidiServiceImpl;
+import org.proxectopuding.gui.model.services.impl.mocks.DeviceManagerServiceMockImpl;
+import org.proxectopuding.gui.model.utils.ConfigurationManager;
+import org.proxectopuding.gui.model.utils.DeviceManager;
+import org.proxectopuding.gui.model.utils.FileDownload;
+import org.proxectopuding.gui.model.utils.FileDownloader;
+import org.proxectopuding.gui.model.utils.FileUtils;
+import org.proxectopuding.gui.model.utils.I18nManager;
+import org.proxectopuding.gui.model.utils.MidiUtils;
+import org.proxectopuding.gui.model.utils.OperativeSystemManager;
+import org.proxectopuding.gui.model.utils.PropertiesManager;
+import org.proxectopuding.gui.model.utils.SoundFontManager;
+import org.proxectopuding.gui.model.utils.connection.ConnectionManager;
+import org.proxectopuding.gui.model.utils.connection.ConnectionManagerJsscImpl;
+
+public class ConfigurationApplicationServiceAcceptanceMockTest {
+	
+	private FileUtils fileUtils = new FileUtils();
+	private FileDownload fileDownload = new FileDownload(fileUtils);
+	private FileDownloader fileDownloader = new FileDownloader(fileDownload);
+	private SoundFontManager soundFontManager =
+			new SoundFontManager(fileDownloader, fileUtils);
+	private OperativeSystemManager operativeSystemManager =
+			new OperativeSystemManager();
+	private MidiUtils midiUtils = new MidiUtils();
+	private MidiServerGeneral midiServerGeneral =
+			new MidiServerGeneral(soundFontManager, operativeSystemManager,
+					fileUtils, midiUtils);
+	private MidiServer midiServer = new MidiServerUnix(midiServerGeneral);
+	private MidiService midiService = new MidiServiceImpl(midiServer);
+	private ConnectionManager connectionManager =
+			new ConnectionManagerJsscImpl(operativeSystemManager, midiService);
+	private DeviceManager deviceManager = new DeviceManager();
+	private DeviceManagerService deviceManagerService =
+			new DeviceManagerServiceMockImpl(connectionManager, deviceManager);
+	
+	private PropertiesManager propertiesManager = new PropertiesManager();
+	private ConfigurationManager configurationManager =
+			new ConfigurationManager(propertiesManager);
+	private I18nManager i18nManager =
+			new I18nManager(configurationManager, operativeSystemManager,
+					propertiesManager);
+	private ConfigurationApplicationService confAppService =
+			new ConfigurationApplicationServiceImpl(i18nManager,
+					deviceManagerService);
+	
+	/**
+	 * Feature: user configures a device
+	 */
+	
+	/**
+	 * Scenario: user applies a configuration
+	 * Given user change some MIDI related value
+	 * When user applies a configuration
+	 * Then MIDI service is reconfigured
+	 */
+	@Test
+	public void applyConfiguration() {
+		
+		// Given
+		int expectedTuningFrequency = 442;
+		confAppService.setTuningFrequency(expectedTuningFrequency);
+		
+		// When
+		MidiServerConfiguration expectedMidiServerConfiguration =
+				confAppService.getMidiServerConfiguration();
+		assertEquals(expectedTuningFrequency,
+				expectedMidiServerConfiguration.getTuningFrequency());
+		midiService.setConfiguration(expectedMidiServerConfiguration);
+		midiService.restart();
+		
+		// Then
+		MidiServerConfiguration midiServiceConfiguration =
+				midiService.getConfiguration();
+		assertEquals(expectedTuningFrequency,
+				midiServiceConfiguration.getTuningFrequency());
+	}
+}
---
+++ src/gui/src/test/java/org/proxectopuding/gui/services/acceptance/DeviceManagerServiceAcceptanceMockTest.java
@@ -0,0 +1,129 @@
+package org.proxectopuding.gui.services.acceptance;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.Test;
+import org.proxectopuding.gui.model.entities.BagpipeConfiguration;
+import org.proxectopuding.gui.model.entities.BagpipeConfigurationType;
+import org.proxectopuding.gui.model.entities.BagpipeDevice;
+import org.proxectopuding.gui.model.entities.SelectionConfiguration;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServer;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServerGeneral;
+import org.proxectopuding.gui.model.entities.midiServer.MidiServerUnix;
+import org.proxectopuding.gui.model.services.DeviceManagerService;
+import org.proxectopuding.gui.model.services.MidiService;
+import org.proxectopuding.gui.model.services.impl.MidiServiceImpl;
+import org.proxectopuding.gui.model.services.impl.mocks.DeviceManagerServiceMockImpl;
+import org.proxectopuding.gui.model.utils.DeviceManager;
+import org.proxectopuding.gui.model.utils.FileDownload;
+import org.proxectopuding.gui.model.utils.FileDownloader;
+import org.proxectopuding.gui.model.utils.FileUtils;
+import org.proxectopuding.gui.model.utils.MidiUtils;
+import org.proxectopuding.gui.model.utils.OperativeSystemManager;
+import org.proxectopuding.gui.model.utils.SoundFontManager;
+import org.proxectopuding.gui.model.utils.connection.ConnectionManager;
+import org.proxectopuding.gui.model.utils.connection.ConnectionManagerJsscImpl;
+
+public class DeviceManagerServiceAcceptanceMockTest {
+	
+	private FileUtils fileUtils = new FileUtils();
+	private FileDownload fileDownload = new FileDownload(fileUtils);
+	private FileDownloader fileDownloader = new FileDownloader(fileDownload);
+	private SoundFontManager soundFontManager =
+			new SoundFontManager(fileDownloader, fileUtils);
+	private OperativeSystemManager operativeSystemManager =
+			new OperativeSystemManager();
+	private MidiUtils midiUtils = new MidiUtils();
+	private MidiServerGeneral midiServerGeneral =
+			new MidiServerGeneral(soundFontManager, operativeSystemManager,
+					fileUtils, midiUtils);
+	private MidiServer midiServer = new MidiServerUnix(midiServerGeneral);
+	private MidiService midiService = new MidiServiceImpl(midiServer);
+	private ConnectionManager connectionManager =
+			new ConnectionManagerJsscImpl(operativeSystemManager, midiService);
+	private DeviceManager deviceManager = new DeviceManager();
+	private DeviceManagerService deviceManagerService =
+			new DeviceManagerServiceMockImpl(connectionManager, deviceManager);
+
+	/**
+	 * Feature: user configures a device
+	 */
+	
+	/**
+	 * Scenario: user selects a device
+	 * Given devices have been found
+	 * When user selects a device
+	 * Then device's configurations are loaded
+	 * And device figures out as selected
+	 */
+	@Test
+	public void selectDevice() {
+		
+		// Given
+		Set<BagpipeDevice> devices = deviceManagerService.findBagpipeDevices();
+		assertTrue(devices.size() > 0);
+		String expectedProductId = devices.iterator().next().getProductId();
+		
+		// When
+		deviceManagerService.setSelectedBagpipeDevice(expectedProductId);
+		Set<BagpipeConfiguration> configurations =
+				deviceManagerService.findBagpipeConfigurations(expectedProductId);
+		
+		// Then
+		BagpipeDevice device = deviceManagerService.getSelectedBagpipeDevice();
+		assertEquals(expectedProductId, device.getProductId());
+		assertEquals(BagpipeConfigurationType.values().length,
+				configurations.size());
+		configurations.forEach(configuration ->
+				assertEquals(expectedProductId, configuration.getProductId()));
+	}
+	
+	/**
+	 * Scenario: user applies a configuration
+	 * Given device is selected
+	 * And device is configured
+	 * When user applies a configuration
+	 * Then device is reconfigured
+	 */
+	@Test
+	public void applyConfiguration() {
+		
+		// Given
+		Set<BagpipeDevice> devices = deviceManagerService.findBagpipeDevices();
+		assertTrue(devices.size() > 0);
+		String expectedProductId = devices.iterator().next().getProductId();
+		deviceManagerService.setSelectedBagpipeDevice(expectedProductId);
+		Set<BagpipeConfiguration> configurations =
+				deviceManagerService.findBagpipeConfigurations(expectedProductId);
+		BagpipeDevice device = deviceManagerService.getSelectedBagpipeDevice();
+		assertEquals(expectedProductId, device.getProductId());
+		assertEquals(BagpipeConfigurationType.values().length,
+				configurations.size());
+		configurations.forEach(configuration ->
+				assertEquals(expectedProductId, configuration.getProductId()));
+		BagpipeConfigurationType expectedConfigurationType =
+				BagpipeConfigurationType.SELECT;
+		BagpipeConfiguration expectedConfiguration = deviceManagerService
+				.getBagpipeConfiguration(expectedProductId,
+						expectedConfigurationType.name());
+		SelectionConfiguration expectedConfigurationData =
+				(SelectionConfiguration) expectedConfiguration;
+		expectedConfigurationData.setBagEnabled(false);
+		
+		// When
+		deviceManagerService.sendBagpipeConfiguration(expectedConfiguration);
+		
+		// Then
+		BagpipeConfiguration configuration = deviceManagerService
+				.findBagpipeConfiguration(expectedConfiguration);
+		assertEquals(expectedProductId, configuration.getProductId());
+		assertEquals(expectedConfigurationType.name(), configuration.getType());
+		SelectionConfiguration configurationData =
+				(SelectionConfiguration) configuration;
+		assertEquals(expectedConfigurationData.isBagEnabled(),
+				configurationData.isBagEnabled());
+	}
+}

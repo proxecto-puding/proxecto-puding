ce46292da5634cc371fb71da494d623ced8c553c
Alejo Pacín Jul <alejopj@gmail.com>
2018-09-01 19:31:07 +0200

    Add installation and execution scripts

---
+++ src/gui/src/main/resources/scripts/install-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get install openjdk-11-jdk
+apt-get install timidity
---
+++ src/gui/src/main/resources/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir ~/proxecto-puding
+cp ./gui-0.0.1-SNAPSHOT.jar ~/proxecto-puding
+cp ./run.sh ~/proxecto-puding

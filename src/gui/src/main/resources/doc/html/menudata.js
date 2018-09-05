812174de34afd73c1dcf4ecd78bbbe6dfe4a5c24
Alejo Pacín Jul <alejopj@gmail.com>
2018-09-05 20:28:33 +0200

    Update configuration application API doc

---
+++ src/gui/src/main/resources/doc/html/menudata.js
@@ -0,0 +1,31 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Classes",url:"annotated.html",children:[
+{text:"Class List",url:"annotated.html"},
+{text:"Class Index",url:"classes.html"},
+{text:"Class Hierarchy",url:"inherits.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions_c.html#index_c"},
+{text:"d",url:"functions_d.html#index_d"},
+{text:"f",url:"functions_f.html#index_f"},
+{text:"g",url:"functions_g.html#index_g"},
+{text:"i",url:"functions_i.html#index_i"},
+{text:"m",url:"functions_m.html#index_m"},
+{text:"o",url:"functions_o.html#index_o"},
+{text:"r",url:"functions_r.html#index_r"},
+{text:"s",url:"functions_s.html#index_s"},
+{text:"w",url:"functions_w.html#index_w"}]},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"a",url:"functions_func.html#index_a"},
+{text:"c",url:"functions_func_c.html#index_c"},
+{text:"d",url:"functions_func_d.html#index_d"},
+{text:"f",url:"functions_func_f.html#index_f"},
+{text:"g",url:"functions_func_g.html#index_g"},
+{text:"i",url:"functions_func_i.html#index_i"},
+{text:"m",url:"functions_func_m.html#index_m"},
+{text:"o",url:"functions_func_o.html#index_o"},
+{text:"r",url:"functions_func_r.html#index_r"},
+{text:"s",url:"functions_func_s.html#index_s"},
+{text:"w",url:"functions_func_w.html#index_w"}]}]}]}]}

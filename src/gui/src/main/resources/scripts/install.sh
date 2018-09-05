fb92297700694a706a521b8520d22ad9ee4e29f0
Alejo Pacín Jul <alejopj@gmail.com>
2018-09-05 23:43:55 +0200

    Update installation and execution scripts

--- src/gui/src/main/resources/scripts/install.sh
+++ src/gui/src/main/resources/scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf ~/proxecto-puding
 mkdir ~/proxecto-puding
-cp ./gui-0.0.1-SNAPSHOT.jar ~/proxecto-puding
-cp ./run.sh ~/proxecto-puding
+cp gui-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/proxecto-puding
+cp run.sh ~/proxecto-puding
+cd ~/proxecto-puding
+jar -xf gui-0.0.1-SNAPSHOT-jar-with-dependencies.jar
--- src/gui/src/main/resources/scripts/run.sh
+++ src/gui/src/main/resources/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar gui-0.0.1-SNAPSHOT.jar
+java org.proxectopuding.gui.ProxectoPuding

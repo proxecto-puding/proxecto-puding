edb1316dd9e939690ac6688f0239760ff71ad0b0
Alejo Pacín Jul <alejopj@gmail.com>
2018-10-05 20:57:25 +0200

    Add missing license files

--- src/hw/SimplePuding/SimplePuding.ino
+++ src/hw/SimplePuding/SimplePuding.ino
@@ -1,20 +1,19 @@
-/* Proxecto Puding (proxecto-puding) - Punteiro Dixital Integramente Galego
- * Advanced wireless digital Galician bagpipes
- * http://proxecto-puding.org, info@proxecto-puding.org
- * Copyright (C) 2012  Alejo Pacín Jul
+/*
+ * Proxecto Puding
+ * Copyright (C) 2011 Alejo Pacín <info@proxecto-puding.org>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /** @file SimplePuding.ino
